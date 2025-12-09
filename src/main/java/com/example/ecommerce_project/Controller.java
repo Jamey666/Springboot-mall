082bdd6264e39b023e149c0943cb86da054dd250
Jamey666 <jamey0603@gmail.com>
2025-12-10 07:48:04 +0800

    Try to Add My Code(intelliJ) To Git

---
+++ src/main/java/com/example/ecommerce_project/Controller.java
@@ -0,0 +1,21 @@
+package com.example.ecommerce_project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    @GetMapping("/test")
+    public Student test(@RequestParam Integer id){
+        String sql = "select * from student where id = :id";
+        MapSqlParameterSource map =  new MapSqlParameterSource();
+        map.addValue("id",id);
+        return namedParameterJdbcTemplate.queryForObject(sql,map,new StudentMapper());
+    }
+}
