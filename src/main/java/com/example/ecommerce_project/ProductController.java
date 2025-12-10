62748654840ec0627c3d8f5dff984523d390f78c
Jamey666 <jamey0603@gmail.com>
2025-12-10 08:27:10 +0800

    查詢商品功能

--- src/main/java/com/example/ecommerce_project/ProductController.java
+++ src/main/java/com/example/ecommerce_project/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce_project;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -8,14 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class ProductController {
     @Autowired
     NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    @GetMapping("/test")
-    public Student test(@RequestParam Integer id){
-        String sql = "select * from student where id = :id";
-        MapSqlParameterSource map =  new MapSqlParameterSource();
-        map.addValue("id",id);
-        return namedParameterJdbcTemplate.queryForObject(sql,map,new StudentMapper());
+    @GetMapping("/test/get")
+    public Product get(@RequestParam Integer id) {
+        String sql = "select * from product where product_id = :id";
+        MapSqlParameterSource map = new MapSqlParameterSource();
+        map.addValue("id", id);
+        return namedParameterJdbcTemplate.queryForObject(sql,map,new ProductMapper());
     }
 }
---
+++ src/main/java/com/example/ecommerce_project/ProductMapper.java
@@ -0,0 +1,22 @@
+package com.example.ecommerce_project;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductMapper implements RowMapper<Product> {
+    public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Product product = new Product();
+        product.setProduct_id(rs.getInt("product_id"));
+        product.setProduct_name(rs.getString("product_name"));
+        product.setCategory(rs.getString("category"));
+        product.setImage_url(rs.getString("image_url"));
+        product.getPrice(rs.getInt("price"));
+        product.setStock(rs.getInt("stock"));
+        product.setDescription(rs.getString("description"));
+        product.setCreated_date(rs.getTimestamp("created_date"));
+        product.setLast_modified_date(rs.getTimestamp("last_modified_date"));
+        return product;
+    }
+}
